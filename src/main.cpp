e4fd1ea82c0e8336229057b36e4bac8646c936b4
George V. <georgev22.github@gmail.com>
2025-03-28 11:17:32 +0200

    feat: replace InputHandler with VirtualInputProxy
    - Remove InputHandler.cpp/h and implement VirtualInputProxy.cpp
    - Add virtual input device support with uinput
    - Improve event handling with callback system
    - Update CMakeLists.txt and main.cpp to reflect changes
    - Modify push_to_talk.cpp to use new VirtualInputProxy

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "InputHandler.h"
 #include "push_to_talk.h"
 #include "Utility.h"
 #include "gui/SettingsGUI.h"
 
 
 int main(const int argc, char *argv[]) {
+    Utility::settings.loadSettings();
     for (int i = 1; i < argc; ++i) {
         if (std::string arg = argv[i]; arg == "--debug") {
             Utility::debug = true;
--- src/push_to_talk.cpp
+++ src/push_to_talk.cpp
@@ -1,18 +1,27 @@
 #include "push_to_talk.h"
 
-#include "InputHandler.h"
+#include "VirtualInputProxy.cpp"
 #include "Utility.h"
 
 PushToTalk::PushToTalk() { Utility::initAudioSystem(); }
 PushToTalk::~PushToTalk() { Utility::cleanupAudioSystem(); }
 
 int PushToTalk::run() {
-    InputHandler handler;
+    try {
+        VirtualInputProxy proxy(Utility::settings.sDevice, Utility::settings.sButton2);
 
-    if (!handler.initializeDevice()) {
-        return -1;
+        proxy.set_callback([](const bool pressed) {
+            Utility::debugPrint("Button 2 " + std::string(pressed ? "pressed" : "released"));
+
+            Utility::playSound((!pressed ? Utility::settings.sPttOffPath : Utility::settings.sPttOnPath).c_str());
+            Utility::setMicMute(!pressed);
+        });
+
+        proxy.start();
+
+        while (true) std::this_thread::sleep_for(std::chrono::seconds(1));
+    } catch (const std::exception &e) {
+        Utility::error("Error: " + std::string(e.what()));
+        return 1;
     }
-
-    handler.processEvents();
-    return 0;
 }
