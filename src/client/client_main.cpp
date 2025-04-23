054537e41022b47a1eaad54429e2b24eb00d311c
George V. <georgev22.github@gmail.com>
2025-04-23 18:03:29 +0300

    style(client): adjust pointer and reference syntax
    - Update pointer declaration style from `char* argv[]` to `char *argv[]`
    - Change reference operator spacing from `auto&` to `auto &`
    - Maintain consistency with project coding standards

--- src/client/client_main.cpp
+++ src/client/client_main.cpp
@@ -1,7 +1,7 @@
 #include "PushToTalkApp.h"
 
-int main(const int argc, char* argv[]) {
-    auto& app = PushToTalkApp::getInstance(argc, argv);
+int main(const int argc, char *argv[]) {
+    auto &app = PushToTalkApp::getInstance(argc, argv);
     app.run();
     return 0;
 }
